6c1f5fb07ad096a92dd00805d5760a4daba322ee
Portfolio <jatindubey646@gmail.com>
2026-04-17 07:14:13 +0530

    Added sign in logics in controllers

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const registering = async (req,res,next){
+const registering = async (req,res,next) => {
     const {username,email,phone,password} = req.body
 
     try{
@@ -26,4 +26,31 @@ const registering = async (req,res,next){
         console.error(error)
         next(error) // error sent to middleware
     }
+}
+
+// Login
+const login = async (res,req,next) => {
+    try{
+        // 1. userExists or not
+    const userExists = await User.findOne({email})
+    if(!userExists){
+        return res.status(400).json({message: "User does not exists"})
+    }
+    // 2. password comparision ( user model )
+    const isMatch = await comparePassword(password)
+    if(!Match){
+        return res.status(400).json({message: "Invalid Credentials"})
+    }
+    // 3.  generate Token
+    const token = await User.generateToken()
+    // 4.set cookie
+    res.cookie("token",token,{
+        httpOnly: true,
+        sameSite: lax,
+    })
+    }  catch(error){
+        console.error(error)
+        next(error)
+    }
+    
 }
